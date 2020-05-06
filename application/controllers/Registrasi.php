14e0034e60f80147755331fbe72c4a066f5f0005
mrizkyff <muhamadrizkyff@gmail.com>
2020-05-06 10:29:16 +0700

    fitur registrasi user

--- application/controllers/Registrasi.php
+++ application/controllers/Registrasi.php
@@ -11,8 +11,57 @@
 			$this->load->view('public/template/footer');
 			$this->load->view('public/script/Registrasi');
         }
-        public function daftar(){
-            
+        public function do_upload()
+        {
+
+            $tanggal = date('Y-m-d H:i:s');
+            $f_name = $this->input->post('first_name');
+            $l_name = $this->input->post('last_name');
+            $username = $this->input->post('username');
+            $gender = $this->input->post('gender');
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            $telp = $this->input->post('telp');
+            $alamat = $this->input->post('alamat');
+
+                $config['upload_path']          = './asset/img/user/';
+                $config['allowed_types']        = 'jpg|png|jpeg';
+                $config['encrypt_name']        = TRUE;
+               
+
+                
+                
+                $this->load->library('upload', $config);
+                if($this->upload->do_upload('foto')){
+                    $upload_data = $this->upload->data();
+                    
+                    
+                    $nama = $upload_data['file_name'];
+                    
+                    $data = array(
+                        'f_name' => $f_name,
+                        'l_name' => $l_name,
+                        'username' => $username,
+                        'password' => $password,
+                        'email' => $email,
+                        'telp' => $telp,
+                        'level' => '2',
+                        'alamat' => $alamat,
+                        'tgl_registrasi' => $tanggal,
+                        'foto' => $nama
+                    );
+                    $this->Registrasi->save_user($data);
+                    // // echo json_encode($hasil);
+                    ?>
+                    <script type="text/javascript">
+                        alert("Registrasi berhasil! Silakan login");
+                        window.location.href = "<?= base_url('main')?>";
+                    </script>
+                    <?php
+                }
+                else{
+                    echo 'gagal';
+                }
         }
     }
     
--- application/models/M_Registrasi.php
+++ application/models/M_Registrasi.php
@@ -1,8 +1,8 @@
 <?php
     class M_Registrasi extends CI_Model
     {
-        public function register(){
-            
+        public function save_user($data){
+            return $this->db->insert('tb_user',$data);
         }
     }
     
