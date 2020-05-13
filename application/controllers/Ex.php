28e30c4b9f4510c2a03694bc3ed91d6a929b647b
mrizkyff <muhamadrizkyff@gmail.com>
2020-05-13 14:34:07 +0700

    percobaan

--- application/controllers/Ex.php
+++ application/controllers/Ex.php
@@ -3,6 +3,7 @@
         public function __construct(){
             parent::__construct();
             $this->load->model('M_Main','Main');
+            $this->load->model('M_Logsys','Logs');
         }
         public function index(){
             // $stok = $this->Main->get_menu_by_code('3')[0]["stok"];
@@ -10,7 +11,8 @@
             // if($this->Main->update_stok('3',$stok)){
             //     var_dump($stok);
             // };
-            var_dump($this->session->userdata());
+            // var_dump($this->session->userdata());
+            echo '<pre>' ,var_dump($this->Logs->get_all_log()) ,'</pre>';
         }
     }
 ?>
