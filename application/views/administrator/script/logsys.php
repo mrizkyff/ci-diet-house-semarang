9aaf1256cdb432b624652831bd545ecdf21be5f6
mrizkyff <muhamadrizkyff@gmail.com>
2020-05-13 14:36:51 +0700

    penyesuaian ajax dengan tabel yang sudah direlasikan

--- application/views/administrator/script/logsys.php
+++ application/views/administrator/script/logsys.php
@@ -17,18 +17,14 @@
                                         '<td>'+(i+1)+'</td>'+
                                         '<td>'+data[i].id_log+'</td>'+
                                         '<td>'+data[i].id_transaksi+'</td>'+
-                                        '<td>'+data[i].username+'</td>'+
+                                        '<td>'+data[i].act_do+'</td>'+
+                                        '<td>'+data[i].buyerr+'</td>'+
                                         '<td>'+data[i].nmbrg+'</td>'+
-                                        '<td>'+jmlJual+'</td>'+
+                                        '<td>'+data[i].jmlJual+'</td>'+
                                         '<td>'+data[i].action+'</td>'+
                                         '<td>'+data[i].tgl_action+'</td>'+
-                                        '<td style "text-align:right;">'+
-                                            tombol1+' '+
-                                            tombol2+' '+
-                                            '<a href="javascript:;" class="btn btn-danger btn-xs item_decline" id="'+data[i].id_transaksi+'" jml="'+data[i].jmlJual+'"> <i class="fas fa-trash"></i> Decline </a>'+' '+
-                                        '</td>'+
                                     '</tr>';
-                        }
+                    }
                         $('#show_logSys').html(html);
                 }
             });
