20426c662d44aa7cec31c1f41f6ea8c0d9b95d1d
mrizkyff <muhamadrizkyff@gmail.com>
2020-05-19 19:08:33 +0700

    membuat halaman setting program

--- application/controllers/Administrator.php
+++ application/controllers/Administrator.php
@@ -37,6 +37,13 @@ class Administrator extends CI_Controller
         $this->load->view('administrator/templates/adm_footer');
         $this->load->view('administrator/script/logsys');
     }
+    public function setting(){
+        $this->load->view('administrator/templates/adm_header');
+        $this->load->view('administrator/templates/adm_sidebar');
+        $this->load->view('administrator/pages/adm_setting');
+        $this->load->view('administrator/templates/adm_footer');
+        $this->load->view('administrator/script/setting');
+    }
         
 }
 
