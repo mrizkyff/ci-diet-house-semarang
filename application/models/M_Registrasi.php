974be045f3dae83c470cc4c0cf0e72fa426d900e
mrizkyff <muhamadrizkyff@gmail.com>
2020-05-22 15:04:57 +0700

    penyesuaian model registrasi dengan rest api

--- application/models/M_Registrasi.php
+++ application/models/M_Registrasi.php
@@ -4,6 +4,14 @@
         public function save_user($data){
             return $this->db->insert('tb_user',$data);
         }
+        public function createUser($data){
+            $this->db->insert('tb_user',$data);
+            return $this->db->affected_rows();
+        }
+        public function updateUser($data,$id){
+            $this->db->update('tb_user',$data,['id_user' => $id]);
+            return $this->db->affected_rows();
+        }
     }
     
 ?>
