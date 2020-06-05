d571473f5b5feee8e25a6865a4ce1608a11bdec1
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-05 21:34:58 +0700

    script action dipindah di bawah footer

--- application/views/public/template/footer.php
+++ application/views/public/template/footer.php
@@ -57,7 +57,86 @@
     <script src="<?php echo base_url() ?>/asset/js/popper.min.js"></script>
     <script src="<?php echo base_url() ?>/asset/js/bootstrap.min.js"></script> -->
 
-	<!-- javasccript cdn -->
-<!-- <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+<script>
+	    $(document).ready(function () {
+            tampilProfile();
+            
+            function tampilProfile(){
+                var id = <?= json_encode($this->session->userdata('idUser'))?>
+
+                $.ajax({
+                    type: "POST",
+                    url: "<?= base_url('profile/getProfile')?>",
+                    data: {id:id},
+                    dataType: "JSON",
+                    success: function (response) {
+                        var lvl = ''
+                        var stat = ''
+                        if(response['level']==1){
+                            lvl = 'Admin'
+                        }
+                        else if(response['level']==2){
+                            lvl = "User"
+                        }
+
+                        if(response['status'] == 1){
+                            stat = 'Active'
+                        }
+                        else if(response['status'] == 2){
+                            stat = 'Non active'
+                        }
+                        else if(response['status'] == 3){
+                            stat = "Suspended"
+                        }
+
+                        $('#fname').val(response['f_name'])
+                        $('#lname').val(response['l_name'])
+                        $('#username').val(response['username'])
+                        $('#password').val(response['password'])
+                        $('#email').val(response['email'])
+                        $('#telp').val(response['telp'])
+                        $('#alamat').val(response['alamat'])
+                        $('#level').val(lvl)
+                        $('#status').val(stat)
+                    }
+                });
+            }
+            
+            $(document).ready(function(){ 
+                // upload foto
+                $('#submit').submit(function(e){
+                    e.preventDefault(); 
+                    var id = $('#idUser').val();
+                        // $.ajax({
+                        //     url:'<?php echo base_url();?>profile/do_upload',
+                        //     type:"post",
+                        //     data:new FormData(this),
+                        //     processData:false,
+                        //     contentType:false,
+                        //     cache:false,
+                        //     async:false,
+                        //     success: function(data){
+                        //         // console.log(data)
+                                
+                        //     }
+                        // });
+                        $.ajax({
+                            type: "POST",
+                            url: "<?= base_url('profile/do_upload')?>",
+                            data: new FormData(this),
+                            processData: false,
+                            contentType: false,
+                            cache: false,
+                            asycn: false,
+                            success: function (response) {
+                                alert('Profil berhasil diperbarui, silakan login!');
+                                // console.log(response);
+                                // tampilProfile();
+                                window.location = "<?= base_url('Login') ?>";
+                            }
+                        });
+                    });
+
+                });
+        });
+</script>
