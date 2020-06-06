001c691f3fe040f0659ab6a310d92fe9686d7192
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-06 16:29:17 +0700

    membuat halaman Cart

---
+++ application/views/public/page/pageCart.php
@@ -0,0 +1,46 @@
+    <!-- awal jumbotron -->
+    <div class="jumbotron bg-light text-dark text-left">
+    	<div class="container">
+            <!-- card transaksi -->
+                <div class="card">
+                <h1 class="text-center">Riwayat Pembelian</h1>
+                <div class="card-body">
+                
+                <!-- tabel transaksi -->
+                <table id="tableTransaksi" class="table table-stripped">
+                <thead>
+                    <th>No</th>
+                    <th>ID Transaksi</th>
+                    <th>Nama Produk</th>
+                    <th>Buyer</th>
+                    <th>Jumlah</th>
+                    <th>Status</th>
+                    <th>Pengiriman</th>
+                    <th>Date</th>
+                </thead>
+                <tbody id="show_transaksi">
+                </tbody>
+                <tfoot>
+                    <th>No</th>
+                    <th>ID Transaksi</th>
+                    <th>Nama Produk</th>
+                    <th>Buyer</th>
+                    <th>Jumlah</th>
+                    <th>Status</th>
+                    <th>Pengiriman</th>
+                    <th>Date</th>
+                </tfoot>
+                </table>
+                <!-- akhir tabel transaksi -->
+                </div>
+            </div>
+                <br>
+                <br>
+            <!-- akhir card transaksi -->
+        </div>
+    </div>
+    <!-- akhir jumbotron -->
+
+
+   
+	</p></div></div></div></p>
