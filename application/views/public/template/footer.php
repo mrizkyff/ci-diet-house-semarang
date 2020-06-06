3a3e542a71bfafd4a35d8db46c8be80290033844
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-06 16:28:42 +0700

    panggil cdn datatable

--- application/views/public/template/footer.php
+++ application/views/public/template/footer.php
@@ -57,6 +57,14 @@
     <script src="<?php echo base_url() ?>/asset/js/popper.min.js"></script>
     <script src="<?php echo base_url() ?>/asset/js/bootstrap.min.js"></script> -->
 
+
+    <!-- DataTables -->
+    <!-- javasccript cdn -->
+    <!-- <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script> -->
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script> -->
+
 <script>
 	    $(document).ready(function () {
             tampilProfile();
