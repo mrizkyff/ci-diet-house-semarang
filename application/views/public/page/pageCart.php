a0f7feede8a9911d645782897a8131cd26eddc6d
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-10 03:45:19 +0700

    udpate pageCart

--- application/views/public/page/pageCart.php
+++ application/views/public/page/pageCart.php
@@ -28,15 +28,43 @@
                 </tfoot>
                 </table>
                 <!-- akhir tabel transaksi -->
+                    <!-- tombol checkout -->
+                    <button type="button" class="btn btn-warning text-light" style="float:right;" id="btnCheckout">Checkout</button>
+                    <!-- tombol checkout -->
                 </div>
             </div>
                 <br>
-                <br>
             <!-- akhir card transaksi -->
+            
+            <br>
         </div>
+        <br>
     </div>
     <!-- akhir jumbotron -->
 
+
+    <!-- modal upload bukti tf -->
+    <!-- Modal -->
+    <div class="modal fade" id="modalUpload" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Upload Bukti Transfer</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+                <div class="modal-body">
+                    Body
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-warning">Kirim</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- modal upload bukti tf -->
 
    
 	</p></div></div></div></p>
