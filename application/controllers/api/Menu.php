db03a5934dda4176c1ff850afe9a96b60b882d30
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-18 14:28:46 +0700

    method beli rest api

--- application/controllers/api/Menu.php
+++ application/controllers/api/Menu.php
@@ -66,6 +66,57 @@ class Menu extends REST_Controller
             }
         }
     }
+    public function beli_get(){
+        $id_user = $this->get('id_user');
+        $id_produk = $this->get('id_produk');
+        $qty = $this->get('quantity');
+        $tanggal = date('Y-m-d H:i:s');
+        
+        $data = array(
+            'id_produk' => $id_produk,
+            'id_user' => $id_user,
+            'stat' => '1',
+            'jmlJual' => $qty,
+            'tgl_transaksi' => $tanggal
+        );
+
+        if($id_user === null){
+            // id not found
+            $this->response([
+                'status' => false,
+                'message' => 'harus login, id session tidak ada'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        else{
+            if($id_produk === null){
+                // id not found
+                $this->response([
+                    'status' => false,
+                    'message' => 'id produk tidak ditemukan'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
+            else{
+                if($this->menu->buy($data)){
+                    $stok = ($this->menu->get_menu_by_code($id_produk)[0]['stok']);
+                    $stok = $stok - $qty;
+                    $this->menu->update_stok($id_produk,$stok);
+                    $this->response([
+                        'status' => true,
+                        'message' => 'pembelian sukses'
+                    ], REST_Controller::HTTP_OK);
+                }
+                else{
+                    $this->response([
+                        'status' => false,
+                        'message' => 'gagal beli'
+                    ], REST_Controller::HTTP_BAD_REQUEST);
+                }
+            }
+        }
+    }
+    public function category_get(){
+        
+    }
 }
 
 ?>
