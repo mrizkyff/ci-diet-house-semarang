139dd24c27112426c89884f8d62b84b5d55e6c94
mrizkyff <muhamadrizkyff@gmail.com>
2020-06-18 14:09:38 +0700

    method update profile

--- application/controllers/api/Profile.php
+++ application/controllers/api/Profile.php
@@ -39,7 +39,43 @@ class Profile extends REST_Controller
                 ], REST_Controller::HTTP_NOT_FOUND);
             }
         }
-        
+    }
+    public function profile_put(){
+        $id = $this->put('id');
+        $password = $this->put('password');
+        $email = $this->put('email');
+        $telp = $this->put('telp');
+        $alamat = $this->put('alamat');
+        $data = array(
+            'password' => $password,
+            'email' => $email,
+            'telp' => $telp,
+            'alamat' => $alamat,
+        );
+        if($id === null){
+            $this->response([
+                'status' => false,
+                'message' => 'masukkan id'
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+        else{
+            // var_dump($id, $password, $email, $telp, $alamat);
+            $profile = $this->Profile->update($id, $data);
+            var_dump($profile);
+            // if($profile){
+            //     $this->response([
+            //         'status' => true,
+            //         'data' => $profile,
+            //         'message' => 'update sukses',
+            //     ], REST_Controller::HTTP_OK);
+            // }
+            // else{
+            //     $this->response([
+            //         'status' => false,
+            //         'message' => 'id tidak ditemukan'
+            //     ], REST_Controller::HTTP_NOT_FOUND);
+            // }
+        }
     }
 }
 
